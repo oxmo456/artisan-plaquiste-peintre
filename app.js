8b17298f6d9310d4663027e0440a07dbaad159a0
Nicolas Markovic <nicolas.anto.markovic@gmail.com>
2014-01-21 22:40:22 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const http = require("http");
+const url = require("url");
+const path = require("path");
+const fs = require("fs");
+const DEFAULT_PORT = 9000;
+
+port = process.argv[2] || DEFAULT_PORT;
+
+console.log("?", process.cwd());
+
+
+http.createServer(function (request, response) {
+    const uri = url.parse(request.url).pathname;
+    var filePath = path.join(process.cwd(), "public", uri);
+    fs.exists(filePath, function (exists) {
+        if (exists) {
+            if (fs.statSync(filePath).isDirectory()) {
+                filePath = path.join(filePath, "public", "index.html");
+            }
+
+            console.log(filePath);
+
+
+            fs.readFile(filePath, "binary", function (error, file) {
+                if (error) {
+                    response.writeHead(500, {"Content-Type": "text/plain"});
+                    response.write(error + "\n");
+                    response.end();
+                    return;
+                }
+                response.writeHead(200);
+                response.write(file, "binary");
+                response.end();
+            });
+        } else {
+            response.writeHead(404, {"Content-Type": "text/plain"});
+            response.write("404 Not Found\n");
+            response.end();
+        }
+    });
+}).listen(parseInt(port, 10));
